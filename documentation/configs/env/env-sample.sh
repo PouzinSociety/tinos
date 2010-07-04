5d4002bdb24a540ee354bad8ca4f610fb1d08837
Patsy Phelan <patsy.phelan@gmail.com>
2010-07-05 00:27:56 +0100

    Updates for demo

--- documentation/configs/env/env-sample.sh
+++ documentation/configs/env/env-sample.sh
@@ -1,9 +1,18 @@
-# Development Environment Settings
-export DEV_HOME=$HOME/work/development
+# Development Toolchain Location
+export DEV_HOME=$HOME/development
+
+# TINOS Git Repository
 export GIT_HOME=$HOME/github/tinos
 
 # Java JDK/JRE (OSX)
 export JAVA_HOME=/Library/Java/Home
+
+# Virgo 
+export SERVER_HOME=$DEV_HOME/springsource/virgo-web-server-2.1.0.M01
+export SERVER_EXEC=$SERVER_HOME/bin
+
+# STS
+export STS_HOME=$DEV_HOME/springsource/sts-2.3.2.RELEASE
 
 # Ruby Settings
 export RUBYOPT=rubygems
@@ -13,8 +22,11 @@ export ANT_HOME=$DEV_HOME/build-tools/apache-ant-1.7.1
 export ANT_OPTS="-Xms64m -Xmx512m -XX:PermSize=128m -XX:MaxPermSize=756m"
 export ANT_EXEC=$ANT_HOME/bin
 
-# Setup Path(in the document)
-export PATH=$JAVA_HOME/bin:$ANT_EXEC:$PATH
+# Findbugs
+export FINDBUGS_HOME=$DEV_HOME/build-tools/findbugs-1.3.9
+
+# Setup Path
+export PATH=$JAVA_HOME/bin:$ANT_EXEC:$SERVER_EXEC:$STS_HOME:$FINDBUGS_HOME:$PATH
 
 # Alias
 export EDITOR=vim
