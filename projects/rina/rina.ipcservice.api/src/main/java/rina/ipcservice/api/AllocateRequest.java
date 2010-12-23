a3a3960db0a0372406312da735703d6291da5883
elfiii <eleni.trouva@i2cat.net>
2010-12-23 12:39:55 +0100

    Changed access levels to some fields

---
+++ projects/rina/rina.ipcservice.api/src/main/java/rina/ipcservice/api/AllocateRequest.java
@@ -0,0 +1,66 @@
+package rina.ipcservice.api;
+
+
+public class AllocateRequest {
+
+	private ApplicationProcessNamingInfo requestedAPinfo = null;
+	private int port_id = 0;
+	private QoSCube cube = null;
+	private boolean result = false;
+
+	private boolean validFormat = false;
+	private boolean acceptRequest = false;
+
+	public AllocateRequest(ApplicationProcessNamingInfo requestedAPinfo, int port_id, QoSCube cube, boolean result) {
+		this.requestedAPinfo = requestedAPinfo;
+		this.port_id = port_id;
+		this.cube = cube;
+		this.result = result;
+	}
+	
+	public ApplicationProcessNamingInfo getRequestedAPinfo() {
+		return requestedAPinfo;
+	}
+
+	public void setRequestedAPinfo(ApplicationProcessNamingInfo requestedAPinfo) {
+		this.requestedAPinfo = requestedAPinfo;
+	}
+
+	public int getPort_id() {
+		return port_id;
+	}
+
+	public void setPort_id(int portId) {
+		port_id = portId;
+	}
+
+	public QoSCube getCube() {
+		return cube;
+	}
+
+	public void setCube(QoSCube cube) {
+		this.cube = cube;
+	}
+
+	public boolean isResult() {
+		return result;
+	}
+
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+
+	public boolean isAcceptRequest() {
+		return acceptRequest;
+	}
+
+	public void setAcceptRequest(boolean acceptRequest) {
+		this.acceptRequest = acceptRequest;
+	}
+
+	public boolean isValidFormat(){
+		//TODO: Add the format check
+		return validFormat;
+	}
+
+}
---
+++ projects/rina/rina.ipcservice.api/src/main/java/rina/ipcservice/api/QoSCube.java
@@ -0,0 +1,60 @@
+package rina.ipcservice.api;
+
+import java.util.*;
+
+/**
+ * Definition of QoS cube
+ *
+ */
+public class QoSCube {
+
+	private Map<String, Object> cube = new HashMap<String, Object>();
+
+	public boolean validFormat = false;
+	
+	//Average bandwidth (measured at the application in bits/sec) 
+	public String avgBW = "average bandwidth"; 
+	//Average SDU bandwidth (measured in SDUs/sec)
+	public String avgSDUBW = "average SDU bandwidth";
+	//Peak bandwidth-duration (measured in bits/sec)
+	public String peakBWduration = "peak bandwidth duration";
+	//Peak SDU bandwidth-duration (measured in SDUs/sec)
+	public String peakSDUBWduration = "peak SDU bandwidth duration";
+	//Burst period measured in seconds 
+	public String burstPeriod = "burst period";
+	//Burst duration, measured in fraction of Burst Period
+	public String burstDuration = "burst duration";
+	//Undetected bit error rate measured as a probability
+	public String undetectedBitErrorRate = "undetected bit error rate";
+	//Partial Delivery Ð Can partial SDUs be delivered?
+	public String partialDeliveryAllowed = "partial delivery";
+	//Order Ð Must SDUs be delivered in order? 
+	public String orderRequired = "SDUs delivered in order";
+	//Max allowable gap in SDUs
+	public String maxGapinSDUs = "maximum allowable gap in SDUs";
+		//Delay in secs
+	public String delay = "delay";
+	//Jitter in secs2
+	public String jitter = "jitter";
+
+
+
+	public Map<String, Object> getCube() {
+		return cube;
+	}
+
+
+
+	public void setCube(Map<String, Object> cube) {
+		this.cube = cube;
+	}
+
+
+
+	public boolean isValidFormat(){
+		//TODO: Add the format check
+		return validFormat;
+		
+	}
+
+}
