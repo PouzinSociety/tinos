9454ce8756f21867fbd6dd13a4eece6b2aa28a5b
elfiii <eleni.trouva@i2cat.net>
2011-01-26 16:00:20 +0100

    new file:   ReleaseResources.java

---
+++ projects/rina/rina.flowallocator.api/src/main/java/rina/flowallocator/api/ReleaseResources.java
@@ -0,0 +1,16 @@
+package rina.flowallocator.api;
+
+/**
+ * Interface between FAI and FA
+ * @author elenitrouva
+ *
+ */
+public interface ReleaseResources {
+	
+	/**
+	 * Called by a FAI to forward de-allocate requests to the FA
+	 * @param portId
+	 */
+	public void forwardDeAllocateRequest(int portId) ;
+
+}
