9a42a851ecfd033360362b9bc354da763ec4a0d8
eduardgrasa <eduardgrasa@192.168.254.135>
2011-01-28 15:20:23 +0100

    added initial rina EFCP API (in progress)

---
+++ projects/rina/rina.efcp.api/src/main/java/rina/efcp/api/DataTransferAEInstance.java
@@ -0,0 +1,26 @@
+package rina.efcp.api;
+
+import java.util.List;
+
+/**
+ * Interface to communicate with the DataTrasferAEInstance
+ * @author eduardgrasa
+ *
+ */
+public interface DataTransferAEInstance {
+
+	/**
+	 * A new SDU has been delivered from (N)-port x. This DTP 
+	 * instance will generate the adequate PDUs and post them 
+	 * to the RMT.
+	 * @param sdu
+	 */
+	public void sdusDelivered(List<byte[]> sdus);
+	
+	/**
+	 * A PDU has been delivered from the RMT. This DTP instance 
+	 * will process it.
+	 * @param pdu
+	 */
+	public void pduDelivered(byte[] pdu);
+}
