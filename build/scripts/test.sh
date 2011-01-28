8851aaaaf5ad6212444346d3efaa1db2b2054cfa
Patsy Phelan <patsy.phelan@gmail.com>
2011-01-28 16:36:02 +0000

    typo

--- build/scripts/test.sh
+++ build/scripts/test.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
 # Create a Bundle project & associated Bundle Integration Test project
-ruby create -n proxy -t $HOME/tmp -o org.tssgu -a bundle
+ruby create -n proxy -t $HOME/tmp -o org.tssg -a bundle
 # Create a deployment plan
 ruby create -n proxy -t $HOME/tmp -o org.tssg -a plan
